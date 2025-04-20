5f7b9099a1835b8177f977e5f13c35d13e64c231
AyOhEe <74308087+AyOhEe@users.noreply.github.com>
2025-04-20 20:07:40 +0100

    API endpoints

--- api.py
+++ api.py
@@ -1,10 +1,29 @@
 from typing import Union
-
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
 
+#used to display other pages
+@app.get("/")
+def index():
+    return "Hello, world!"
 
-@app.get("/items/{item_id}")
-async def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+#only ever shows past 24hrs
+@app.get("/uptime_graph")
+def uptime_graph():
+    return "TODO"
+
+#raw data since provided date, up to 31 days in the past
+@app.get("/raw")
+def raw(since: int):
+    return "TODO"
+
+#returns average uptime since provided date
+@app.get("/uptime")
+def uptime(since: int):
+    return "TODO"
+
+#returns a list of all disruptions since provided date
+@app.get("/disruptions")
+def disruptions(since: int):
+    return "TODO"
