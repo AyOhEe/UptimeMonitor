393b95634aa067b1b9c434171bf62c8e3c17d497
AyOhEe <74308087+AyOhEe@users.noreply.github.com>
2025-04-20 20:47:00 +0100

    API endpoints validate provided time periods

--- api.py
+++ api.py
@@ -1,29 +1,30 @@
-from typing import Union
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, Query, HTTPException
+
 
 app = FastAPI()
 
-#used to display other pages
+
+#demo page which uses the endpoints
 @app.get("/")
 def index():
     return "Hello, world!"
 
-#only ever shows past 24hrs
+#shows past 24hrs of uptime on a graph
 @app.get("/uptime_graph")
 def uptime_graph():
     return "TODO"
 
-#raw data since provided date, up to 31 days in the past
+#raw data since provided date, up to 31 days in the past, between now and {period} seconds ago
 @app.get("/raw")
-def raw(since: int):
+def raw(period: int = Query(ge=0, le=31*24*60*60)):
     return "TODO"
 
-#returns average uptime since provided date
+#returns average uptime between now and {period} seconds ago
 @app.get("/uptime")
-def uptime(since: int):
+def uptime(period: int = Query(ge=0)):
     return "TODO"
 
-#returns a list of all disruptions since provided date
+#returns a list of all disruptions between now and {period} seconds ago
 @app.get("/disruptions")
-def disruptions(since: int):
+def disruptions(period: int = Query(ge=0)):
     return "TODO"
