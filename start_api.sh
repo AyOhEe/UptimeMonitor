3aaa8f551fa1df78915824cab02951a6299282db
AyOhEe <74308087+AyOhEe@users.noreply.github.com>
2025-05-01 00:03:17 +0100

    Removed ampersand and redirect from script calls

--- start_api.sh
+++ start_api.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-source UptimeMonitor/bin/activate
 yes | fastapi run api.py > api.log 2> api.log &
 disown
--- start_uptime.sh
+++ start_uptime.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-source UptimeMonitor/bin/activate
 python3 uptime.py --target 8.8.8.8 --period 2000 &
 disown
